e54623cc4d3eae2435fb952fb5d8194650bb37ab
Tuyet Le <tle83@lsu.edu>
2017-03-27 09:39:07 -0500

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+/**
+Tuyet-Ngoc Le
+	CSC 2463
+	Final Project
+**/
+
+function preload(){
+
+}
+
+function setup(){
+	createCanvas(720, 480);
+	background(100);
+}
+
+
+function mousePressed(){
+
+}
+
+function draw(){
+	
+}
+
+function musicNote(){
+
+}
