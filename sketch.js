cea9ce08308b5911edbcc7f45fa2f33e047e7704
tle83 <tnle833@gmail.com>
2017-04-25 17:46:02 -0500

    Add Images

--- sketch.js
+++ sketch.js
@@ -16,7 +16,14 @@ var stop = true;
 var buttonColorPlay;
 var buttonColorRec;
 var buttonColorStop;
-var colorC, colorD, colorE, colorF, colorG, colorA, colorB;
+
+var cNote = false;
+var dNote = false;
+var eNote = false; 
+var fNote = false; 
+var gNote = false; 
+var aNote = false; 
+var bNote = false; 
 
 var songs = [];
 var song1, song2, song3, song4, song5;
@@ -25,8 +32,24 @@ var songPlaying;
 var songCount = 0;
 var playerCorrect = false;
 
+var bg;
+var buttons;
+var music;
+var playButtonON = false;
+var recButtonON = false;
+var checkButtonOn = false;
+
+function preload(){
+	buttons = loadImage("Images/Buttons.png");
+	music = loadImage("Images/music.png");
+}
+
 
 function setup(){
+	//background
+	createCanvas(1280, 720);
+	background(68, 203, 208);
+
 	buttonColorPlay = color(255);
 	buttonColorRec = color(255);
 	buttonColorStop = color(255);
@@ -39,17 +62,16 @@ function setup(){
 	colorA = color(0);
 	colorB = color(0);
 
-	createCanvas(720, 480);
-	background(255);
-
 	playerAnswer = [];
 
 	synth = new Tone.PolySynth(4, Tone.MonoSynth).toMaster();
 
 	singleSynth = new Tone.Synth().toMaster();
 
+	Tone.Transport.loop = true;
+	Tone.Transport.loopEnd = '5m';
 	Tone.Transport.bpm = 100;
-	//Tone.Transport.schedule(triggerSynth, 0);
+	Tone.Transport.schedule(triggerSynth, 0);
 
 	song1 = ['d4', 'e4', 'd4', 'a4', 'g4', 'b4', 'c4', 'g4'];
 	song2 = ['a4', 'g4', 'a4', 'b4', 'd4', 'd4', 'b4', 'g4'];
@@ -60,34 +82,56 @@ function setup(){
 }
 
 function draw(){
-
 	songPlaying = songs[songCount];
 	seq = new Tone.Sequence(synthNotes, songPlaying, '8n');
 	seq.start(0).stop('1m');
 
-	musicLine(40, 230);
-	noteC(70*2);
-	noteD(70*3);
-	noteE(70*4);
-	noteF(70*5);
-	noteG(70*6);
-	noteA(70*7);
-	noteB(70*8);
-
-	if(isPlaying == true){
-		//PLACE PLAYING ANIMATION HERE
+	if(cNote == false){
+		noteC(200, 70, 0);
 	}
-	if( isRecording == true){
-		//PLACE RECORDING ANIMATION HERE
+	if(cNote == true){
+		noteC(200, 70, 96);
 	}
-	if(stop == true){
-		//PLACE IDLE ANIMATION HERE
+	if(dNote == false){
+		noteD(320, 242, 0);
+	}
+	if(dNote == true){
+		noteD(320, 242, 96);
+	}
+	if(eNote == false){
+		noteE(480, 70, 0);
+	}
+	if(eNote == true){
+		noteE(480, 70, 96);
+	}
+	if(fNote == false){
+		noteF(640, 242, 0);
+	}
+	if(fNote == true){
+		noteF(640, 242, 96);
+	}
+	if(gNote == false){
+		noteG(800, 70, 0);
+	}
+	if(gNote == true){
+		noteG(800, 70, 96);
+	}
+	if(aNote == false){
+		noteA(960, 242, 0);
+	}
+	if(aNote == true){
+		noteA(960, 242, 96);
+	}
+	if(bNote == false){
+		noteB(1080, 70, 0);
+	}
+	if(bNote == true){
+		noteB(1080, 70, 96);
 	}
 
 	playButton();
 	recordButton();
 	stopButton();
-
 }
 
 //AUDIO
@@ -110,75 +154,76 @@ function keyPressed(){
 		}
 	}
 	if(keyCode == 67){
-		colorC = color(161, 88, 220);
-		colorD = color(0);
-		colorE = color(0);
-		colorF = color(0);
-		colorG = color(0);
-		colorA = color(0);
-		colorB = color(0);
 		singleSynth.triggerAttackRelease("C4", "8n");
+		cNote = true;
+		dNote = false;
+		eNote = false; 
+		fNote = false; 
+		gNote = false; 
+		aNote = false; 
+		bNote = false; 
+		
 	}
 	else if(keyCode == 68){
-		colorC = color(0);
-		colorD = color(161, 88, 220);
-		colorE = color(0);
-		colorF = color(0);
-		colorG = color(0);
-		colorA = color(0);
-		colorB = color(0);
 		singleSynth.triggerAttackRelease("D4", "8n");
+		cNote = false;
+		dNote = true;
+		eNote = false; 
+		fNote = false; 
+		gNote = false; 
+		aNote = false; 
+		bNote = false; 
 
 	}
 	else if(keyCode == 69){
-		colorC = color(0);
-		colorD = color(0);
-		colorE = color(161, 88, 220);
-		colorF = color(0);
-		colorG = color(0);
-		colorA = color(0);
-		colorB = color(0);
 		singleSynth.triggerAttackRelease("E4", "8n");
+		cNote = false;
+		dNote = false;
+		eNote = true; 
+		fNote = false; 
+		gNote = false; 
+		aNote = false; 
+		bNote = false; 
 	}
 	else if(keyCode == 70){
-		colorC = color(0);
-		colorD = color(0);
-		colorE = color(0);
-		colorF = color(161, 88, 220);
-		colorG = color(0);
-		colorA = color(0);
-		colorB = color(0);
 		singleSynth.triggerAttackRelease("F4", "8n");
+		cNote = false;
+		dNote = false;
+		eNote = false; 
+		fNote = true; 
+		gNote = false; 
+		aNote = false; 
+		bNote = false; 
 	}
 	else if(keyCode == 71){
-		colorC = color(0);
-		colorD = color(0);
-		colorE = color(0);
-		colorF = color(0);
-		colorG = color(161, 88, 220);
-		colorA = color(0);
-		colorB = color(0);
 		singleSynth.triggerAttackRelease("G4", "8n");
+		cNote = false;
+		dNote = false;
+		eNote = false; 
+		fNote = false; 
+		gNote = true; 
+		aNote = false; 
+		bNote = false; 
 	}
 	else if(keyCode == 65){
-		colorC = color(0);
-		colorD = color(0);
-		colorE = color(0);
-		colorF = color(0);
-		colorG = color(0);
-		colorA = color(161, 88, 220);
-		colorB = color(0);
 		singleSynth.triggerAttackRelease("A4", "8n");
+		cNote = false;
+		dNote = false;
+		eNote = false; 
+		fNote = false; 
+		gNote = false; 
+		aNote = true; 
+		bNote = false; 
 	}
 	else if(keyCode == 66){
-		colorC = color(0);
-		colorD = color(0);
-		colorE = color(0);
-		colorF = color(0);
-		colorG = color(0);
-		colorA = color(0);
-		colorB = color(161, 88, 220);
 		singleSynth.triggerAttackRelease("B4", "8n");
+		cNote = false;
+		dNote = false;
+		eNote = false; 
+		fNote = false; 
+		gNote = false; 
+		aNote = false; 
+		bNote = true; 
 	}
 	if(isRecording == true && stop == false){
 		append(playerAnswer, key.toLowerCase() + "4");
@@ -187,44 +232,24 @@ function keyPressed(){
 }
 
 function mousePressed(){
-	if((mouseX > width / 20) && (mouseX < (width / 20) + 50) &&
-		(mouseY > 400) && (mouseY < 400 + 50)){
+	if((mouseX > 20) && (mouseX < 116) &&
+		(mouseY > 570) && (mouseY < 666)){
+			playButtonON = true;
+			recButtonON = false;
+			checkButtonOn = false;
+
 			isPlaying = true;
 			print("Playing");
 
-			buttonColorPlay = color(100);
-			buttonColorRec = color(255);
-			buttonColorStop = color(255);
-
 			Tone.Transport.start();
 			console.log("Notes being played: " + songPlaying);
-/*
-			for(var i = 0; i < songPlaying.length; i++){
-				if(songPlaying[i] == 'c4'){
-					print("c");
-				}
-				if(songPlaying[i]  == 'd4'){
-					print("d");
-				}
-				if(songPlaying[i]  == 'e4'){
-					print("e");
-				}
-				if(songPlaying[i]  == 'f4'){
-					print("f");
-				}
-				if(songPlaying[i]  == 'g4'){
-					print("g");
-				}
-				if(songPlaying[i]  == 'a4'){
-					print("a");
-				}
-				if(songPlaying[i]  == 'b4'){
-					print("b");
-				}
-			}*/
 	}
-	if((mouseX > width / 7) && (mouseX < (width / 7) + 50) &&
-		(mouseY > 400) && (mouseY < 400 + 50)){
+	if((mouseX > 126) && (mouseX < 222) &&
+		(mouseY > 570) && (mouseY < 666)){
+			playButtonON = false;
+			recButtonON = true;
+			checkButtonOn = false;
+
 			isRecording = true;
 			stop = false;
 			
@@ -232,24 +257,21 @@ function mousePressed(){
 			playerAnswer = [];
 			print("Recording");
 
-			buttonColorRec = color(100);
-			buttonColorPlay = color(255);
-			buttonColorStop = color(255);
-
 			console.log("Song Playing: " + songPlaying);
 			console.log("Song Count: " + songCount);
 		}
-	if((mouseX > width / 4.6) && (mouseX < (width / 4.6) + 50) &&
-		(mouseY > 400) && (mouseY < 400 + 50)){
-			stop = true;
-			print("Stopped");
-			buttonColorStop = color(100);
-			buttonColorPlay = color(255);
-			buttonColorRec = color(255);
+	if((mouseX > 232) && (mouseX < 328) &&
+		(mouseY > 570) && (mouseY < 666)){
+			playButtonON = false;
+			recButtonON = false;
+			checkButtonOn = true;
 
+			stop = true;
+			print("Checking");
+			Tone.Transport.stop();
 			console.log("Song Count: " + songCount);
 
-			Tone.Transport.stop();
+			seq.mute = true;
 			var correctCount = 0;
 			if(stop = true){
 				for(var i = 0; i < songPlaying.length; i++){
@@ -273,33 +295,30 @@ function mousePressed(){
 }
 
 function playButton(){
-	fill(buttonColorPlay);
-	stroke(0);
-	strokeWeight(2);
-	rect(width / 20, 400, 50, 50, 10);
-
-	fill(137, 253, 109);
-	triangle(width / 13, 410, width / 13, 440, width / 10, 425);
+	if(playButtonON){
+		image(buttons, 20, 570,  96, 96, 96, 0, 96, 96);
+	}
+	else{
+		image(buttons, 20, 570,  96, 96, 0, 0, 96, 96);
+	}
 }
 
 function recordButton(){
-	fill(buttonColorRec);
-	stroke(0);
-	strokeWeight(2);
-	rect(width / 7.5, 400, 50, 50, 10);
-
-	fill(250, 16, 76);
-	ellipse(width / 6, 425, 30, 30);
+	if(recButtonON){
+		image(buttons, 126, 570, 96, 96, 96, 96, 96, 96);
+	}
+	else{
+		image(buttons, 126, 570, 96, 96, 0, 96, 96, 96);
+	}
 }
 
 function stopButton(){
-	fill(buttonColorStop);
-	stroke(0);
-	strokeWeight(2);
-	rect(width / 4.6, 400, 50, 50, 10);
-
-	fill(250, 16, 76);
-	rect(width / 4.25, 413, 25, 25);
+	if(checkButtonOn){
+		image(buttons, 232, 570, 96, 96, 96, 192, 96, 96);
+	}
+	else{
+		image(buttons, 232, 570, 96, 96, 0, 192, 96, 96);
+	}
 }
 
 function musicNote(x,y, c){
@@ -309,41 +328,31 @@ function musicNote(x,y, c){
 	//rect(x+5, y-35, 20, 8);
 }
 
-function musicLine(x, y){
-	fill(0);
-	noStroke();
-	rect(x, y-100, 600, 3);
-	rect(x, y-125, 600, 3);
-	rect(x, y-150, 600, 3);
-	rect(x, y-175, 600, 3);
-	rect(x, y-200, 600, 3);
-}
-//each note is 40 pixel apart
-function noteC(x){
-	rect(x-20, 155, 40, 3);
-	musicNote(x, 155, colorC);
+
+function noteC(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
 
-function noteD(x){
-	musicNote(x, 145, colorD);
+function noteD(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
 
-function noteE(x){
-	musicNote(x, 130, colorE);
+function noteE(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
 
-function noteF(x){
-	musicNote(x, 120, colorF);
+function noteF(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
 
-function noteG(x){
-	musicNote(x, 105, colorG);
+function noteG(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
 
-function noteA(x){
-	musicNote(x, 95, colorA);
+function noteA(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
 
-function noteB(x){
-	musicNote(x, 80, colorB);
+function noteB(x,y,xTile){
+	image(music, x, y, 96, 96, xTile, 0, 96, 96);
 }
